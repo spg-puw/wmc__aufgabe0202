a9b48165894f34d0daaa3f7b2180c66f338d0e62
spg-puw <117909782+spg-puw@users.noreply.github.com>
2023-09-26 00:35:29 +0200

    remove named exports

--- aufgabe1.mjs
+++ aufgabe1.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 // Define your JSON object.
-export const meeting = {
+const meeting = {
     //TODO
 };
 
@@ -12,7 +12,7 @@ function capitalizeFirstLetter(string) {
 // *************************************************************************************************
 // Wie viele Tage vor dem Meeting wurde die Einladung (invitationSent) gesendet? Schreibe das Ergebnis
 // in die Variable invitationSendBeforeMeeting.
-export function zeitBerechnen(meeting) {
+function zeitBerechnen(meeting) {
     console.log("* Einladung vor dem Meeting ********************************************************");
     const invitationSendBeforeMeeting = 0; //TODO
     console.log(`   Die Einladung wurde ${invitationSendBeforeMeeting.toFixed(2)} Tage vor dem Meeting gesendet.`);
@@ -20,7 +20,7 @@ export function zeitBerechnen(meeting) {
 
 // *************************************************************************************************
 // Weise die Anzahl der Teilnehmer der Variable attendeesCount zu.
-export function anzahlTeilnehmer(meeting) {
+function anzahlTeilnehmer(meeting) {
     console.log("* Anzahl der eingeladenen Teilnehmer ***********************************************");
     const attendeesCount = 0; //TODO
     console.log(`   ${attendeesCount} Personen sind eingeladen.`);
@@ -29,7 +29,7 @@ export function anzahlTeilnehmer(meeting) {
 // *************************************************************************************************
 // Gib für jedes String Property im JSON Object den Wert aus. Verwende dafür eine Schleife, die
 // das Objekt dynamisch durchgeht.
-export function propsAnzeigen(meeting) {
+function propsAnzeigen(meeting) {
     console.log("* String Properties und ihre Werte**************************************************");
     //TODO
 }
@@ -38,7 +38,7 @@ export function propsAnzeigen(meeting) {
 // Wer hat für das Meeting schon zugesagt, d. h. confirmed hat den Wert true? Verwende dafür eine
 // Schleife, die das Array durchgeht. Der Title ist dynamisch auszugeben und nicht fix im Code
 // einzutragen.
-export function zugesagteTeilnehmer(meeting) {
+function zugesagteTeilnehmer(meeting) {
     console.log("* Zugesagte Teilnehmer *************************************************************");
     //TODO
 }
@@ -46,13 +46,12 @@ export function zugesagteTeilnehmer(meeting) {
 // *************************************************************************************************
 // Wer hat für das Meeting definitiv abgesagt, d. h. confirmed hat den Wert false? Verwende dafür
 // eine Schleife, die das Array durchgeht.
-export function abgesagteTeilnehmer(meeting) {
+function abgesagteTeilnehmer(meeting) {
     console.log("* Abgesagte Teilnehmer *************************************************************");
     //TODO
 }
 
 export default { meeting, zeitBerechnen, anzahlTeilnehmer, propsAnzeigen, zugesagteTeilnehmer, abgesagteTeilnehmer }
-
 if (import.meta.url.endsWith(process.argv[1])) {
     zeitBerechnen(meeting);
     anzahlTeilnehmer(meeting);
